987474e9a653063e14cc73c3ba45f620f4e23212
wbeirigo <Wbeirigo@terravisiongeo.com.br>
2015-10-15 17:43:59 -0300

    Teste

--- weather/proj/teste.sh
+++ weather/proj/teste.sh
@@ -1,12 +1,7 @@
-curl \
--H "Content-Type: application/json" \
--X POST \
--d '{"infile":"{xAxis: {categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']},\
-                        series: [{data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]}]}" \,
-   "constr":"chart","outfile":"chart.png"}' \
-tvbhdesenv.terravision.local:3003  
-
-
-
-
+#!/bin/bash
+pip freeze requeriments.txt
+git add .
+git commit -m 'Teste'
+git status
+git push
 
